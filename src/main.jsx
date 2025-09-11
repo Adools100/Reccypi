7810983a282f8d9b54d930217b029d55200a5e95
Adools100 <adoolslimitless@gmail.com>
2025-09-11 16:31:51 -0400

    Added 404 trick

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route, HashRouter } from 'react-router-dom';
 import HomePage from './HomePage.jsx';
 import RecipiePage from './RecipiePage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <Routes>
       <Route path='/Reccypi' element= {<HomePage />}></Route>
       <Route path='/Reccypi/recipie/:id' element= {<RecipiePage />}></Route>
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
